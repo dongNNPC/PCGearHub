a9039515835f781989dc6a0732480fa859084d14
KhanhTran <trantankhanh31102003@gmail.com>
2023-07-09 21:44:59 +0700

    merge nhutDong

---
+++ src/main/java/com/poly/asm/controller/ShoppingCartController.java
@@ -0,0 +1,29 @@
+package com.poly.asm.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pcgearhub")
+public class ShoppingCartController {
+    
+
+    @RequestMapping("/cart")
+	public String cart() {
+
+		return "/fragment/ShoppingCart";
+	}
+
+	 @RequestMapping("/confirm-information")
+	public String confirm_information() {
+
+		return "/fragment/confirm-information";
+	}
+
+	@RequestMapping("/Thank-you")
+	public String thanhyou() {
+
+		return "/fragment/thankforShop";
+	}
+}
